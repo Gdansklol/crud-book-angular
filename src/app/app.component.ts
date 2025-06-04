3e3f52b577c6f93026dda482bf0f0c3f6951fb16
swedenebomi <swedenebomi@gamil.com>
2025-06-04 12:40:04 +0200

    Fixade routing och position av footer på alla sidor (responsiv design)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
+// src/app/app.component.ts
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { NavigationBarComponent } from './components/navigation-bar/navigation-bar.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterModule, NavigationBarComponent],
+  imports: [RouterModule, NavigationBarComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
