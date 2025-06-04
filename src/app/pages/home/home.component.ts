a0efff40260369777d995a62db7188a708966297
swedenebomi <swedenebomi@gamil.com>
2025-06-04 16:08:01 +0200

    tog bort banner och lade till välkomsttext,ikon,add btn enligt kraven

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,11 +1,21 @@
 import { Component } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faPlus } from '@fortawesome/free-solid-svg-icons';
+import { FaIconLibrary } from '@fortawesome/angular-fontawesome';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FontAwesomeModule, RouterModule],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {}
+export class HomeComponent {
+  constructor(library: FaIconLibrary) {
+    library.addIcons(faPlus); 
+  }
+
+  faPlus = faPlus; 
+}
